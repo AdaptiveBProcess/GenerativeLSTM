76f0a44b18ab235841a4bbcf5435f50961bac812
Mcamargo85 <38304139+Mcamargo85@users.noreply.github.com>
2020-03-12 14:48:07 +0200

    Role discovery update

--- embedding_training.py
+++ embedding_training.py
@@ -28,13 +28,13 @@ def training_model(parameters):
     parameters['read_options']['filter_d_attrib'] = True
     log = lr.LogReader(os.path.join('input_files', parameters['file_name']), parameters['read_options'])
     # Pre-processing tasks
-    _, resource_table = rl.read_resource_pool(log, sim_percentage=parameters['rp_similarity'])
+    res_analyzer = rl.ResourcePoolAnalyser(log, sim_threshold=parameters['rp_similarity'])
 #    for x in [0.8, 0.85, 0.9]:
 #        resource_pool, resource_table = rl.read_resource_pool(log, sim_percentage=x)
 #        print(pd.DataFrame.from_records(resource_pool))
 
     # Role discovery
-    log_df_resources = pd.DataFrame.from_records(resource_table)
+    log_df_resources = pd.DataFrame.from_records(res_analyzer.resource_table)
     log_df_resources = log_df_resources.rename(index=str, columns={"resource": "user"})
     # Dataframe creation
     log_df = pd.DataFrame.from_records(log.data)
